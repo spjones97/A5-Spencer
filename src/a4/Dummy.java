2bb45027b44348c464429b615e0157e72827567b
Ketan Mayer-Patel <kmp@cs.unc.edu>
2019-02-11 20:56:16 -0500

    Setup with package structure

---
+++ src/a4/Dummy.java
@@ -0,0 +1,11 @@
+package a4;
+
+// This is just a empty dummy class that was included in order to 
+// make sure that the src/a4 directory structure needed for the 
+// a4 package was created as part of the A4 template repository
+// You don't have to do anything with it and can even delete it
+// if you want.
+
+public class Dummy {
+
+}
