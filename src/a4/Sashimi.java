5c7c54aa3f16d7e2ec6759b0387330ac4c0a3212
cchow <cchow@UNC-R90N9CMK.wireless-1x.unc.edu>
2019-04-11 00:42:28 -0400

    sash

--- src/a4/Sashimi.java
+++ src/a4/Sashimi.java
@@ -7,13 +7,41 @@ public class Sashimi implements Sushi{
 	protected IngredientPortion[] ingredientArray;
 	protected IngredientPortion ingredientAmt;
 	
-	public Sashimi(SashimiType type) {
+	public Sashimi(SashimiType _type) {
+		
+		ingredientArray = new IngredientPortion[1];
+		
+		if (_type == SashimiType.TUNA) {
+			ingredientName = "tuna sashimi";
+;			ingredientAmt = new TunaPortion(0.75);
+		} else if (_type == SashimiType.YELLOWTAIL) {
+			ingredientName = "yellowtail sashimi";
+			ingredientAmt = new YellowtailPortion(0.75);
+		} else if (_type == SashimiType.EEL) {
+			ingredientName = "eel sashimi";
+			ingredientAmt = new EelPortion(0.75);
+		} else if (_type == SashimiType.CRAB) {
+			ingredientName = "crab sashimi";
+			ingredientAmt = new CrabPortion(0.75);
+		} else if (_type == SashimiType.SHRIMP) {
+			ingredientName = "shrimp sashimi";
+			ingredientAmt = new ShrimpPortion(0.75);
+		}
+		ingredientArray[0] = ingredientAmt;
+		
+		for (int i=0; i<ingredientArray.length; i++) {
+			if (ingredientArray[i] == null) {
+				throw new RuntimeException("ingredients array can't be null");
+			}
+		}
+		
+
 	}
 
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
-		return this.ingredientName;
+		return ingredientName;
 	}
 
 	@Override
