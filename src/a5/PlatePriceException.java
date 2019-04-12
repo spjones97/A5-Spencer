614de86490ff80d4e37c28ad8931209eff40aa2d
cchow <cchow@UNC-R90N9CMK.wireless-1x.unc.edu>
2019-04-11 23:45:49 -0400

    asdf

--- src/a5/PlatePriceException.java
+++ src/a5/PlatePriceException.java
@@ -4,14 +4,9 @@ public class PlatePriceException extends Exception{
 	
 	private double price;
 	
-	public PlatePriceException(String s) {
-		super("platePrice below 0");
-	}
+
 	public double getIllegalPrice() {
 		return price;
 	}
-	public String getMessage() {
-		return super.getMessage() +" (" + price + " )";
-	}
-	
+
 }
